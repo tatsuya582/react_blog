a3e2b491d54bb406eeed0d5e9b19c381a60278cc
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-09-04 18:08:18 +0900

    add: 雛形を作成

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,8 +1,12 @@
 import './App.css';
+import Main from './components/Main';
+import Sidebar from './components/Sidebar';
 
 function App() {
   return (
     <div className="App">
+      <Sidebar />
+      <Main />
     </div>
   );
 }
