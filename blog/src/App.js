fe0f00aa68f3fe58cad6dc53f5dc42b3895c846a
tatsuya582 <145332193+tatsuya582@users.noreply.github.com>
2024-09-06 12:12:42 +0900

    add: noteがローカルストレージに保存される様に変更

--- blog/src/App.js
+++ blog/src/App.js
@@ -1,4 +1,4 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './App.css';
 import Main from './components/Main';
 import Sidebar from './components/Sidebar';
@@ -6,8 +6,16 @@ import uuid from 'react-uuid'
 
 function App() {
 
-  const [notes, setNotes] = useState([]);
+  const [notes, setNotes] = useState(JSON.parse(localStorage.getItem("notes")) || []);
   const [activeNote, setActiveNote] = useState(false);
+
+  useEffect(() => {
+    localStorage.setItem("notes", JSON.stringify(notes));
+  }, [notes]);
+
+  useEffect(() => {
+      setActiveNote(notes[0]?.id);
+  }, []);
 
   const onAddNote = () => {
     console.log("ノードが追加されました")
